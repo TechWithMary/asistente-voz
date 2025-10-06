8a9da696d96953788b1c08044aa6863e129a68a9
Mary Cardona <mary@blockflux.co>
2025-10-06 14:13:44 -0500

    Uso el modelo Gemini 1.5 Pro latest

--- api/procesar-audio.js
+++ api/procesar-audio.js
@@ -33,7 +33,7 @@ module.exports = async (req, res) => {
         const GOOGLE_PROJECT_ID = process.env.GCLOUD_PROJECT;
         const GOOGLE_LOCATION = process.env.GCLOUD_LOCATION;
 
-        const url = `https://${GOOGLE_LOCATION}-aiplatform.googleapis.com/v1/projects/${GOOGLE_PROJECT_ID}/locations/${GOOGLE_LOCATION}/publishers/google/models/gemini-1.5-pro:streamGenerateContent`;
+        const url = `https://${GOOGLE_LOCATION}-aiplatform.googleapis.com/v1/projects/${GOOGLE_PROJECT_ID}/locations/${GOOGLE_LOCATION}/publishers/google/models/gemini-1.5-pro-latest:streamGenerateContent`;
 
         const prompt = `
             Eres un asistente experto en facturación. Tu tarea es analizar la siguiente nota de voz.
